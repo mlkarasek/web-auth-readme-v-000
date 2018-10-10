ed0144afb114bc1a4fe0bf9343fce22ca9d426ca
Monica  Karasek <mlkarasek@gmail.com>
2018-10-10 10:31:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ def authenticate_user
     redirect_uri = CGI.escape("http://localhost:3000/auth")
     foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
     redirect_to foursquare_url unless logged_in?
-end 
-  
+end
+
   def logged_in?
     !!session[:token]
 end
