095af3f0ffe6e0834049df3d0f6e1e0aa3a1c60c
Monica  Karasek <mlkarasek@gmail.com>
2018-10-10 10:57:46 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -34,7 +34,8 @@ class SearchesController < ApplicationController
     req.params['oauth_token'] = session[:token]
     # don't forget that pesky v param for versioning
     req.params['v'] = '20160201'
+    end
+    @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
   end
-  @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
-  end
+
 end
