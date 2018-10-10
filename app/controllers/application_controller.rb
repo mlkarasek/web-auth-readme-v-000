2e21859abeb2d8b532fd13bf27dab41df9c39c0c
Monica  Karasek <mlkarasek@gmail.com>
2018-10-10 10:58:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user
+
 private
 
   def authenticate_user
@@ -15,4 +16,4 @@ private
   def logged_in?
     !!session[:token]
   end
-end 
+end
